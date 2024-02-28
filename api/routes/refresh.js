38c4c22196b5f38236d32f41d050152d873c0241
Valentin <111338107+vlnt-trsv@users.noreply.github.com>
2024-02-29 03:15:58 +0700

    delete refresh.js

--- api/routes/refresh.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const RefreshTokenController = require("../controllers/refreshToken");
-
-// Маршрут для обновления токена
-router.post("/", RefreshTokenController.handleRefreshToken);
-
-module.exports = router;
